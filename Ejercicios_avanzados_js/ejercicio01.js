c264752de786264006dc0743fff23660456c8fd9
Abel <abel.fernandez.martinez@gmail.com>
2024-07-17 22:32:40 +0200

    Create ejercicio01.js

---
+++ Ejercicios_avanzados_js/ejercicio01.js
@@ -0,0 +1,39 @@
+const movies = [
+  {
+    title: "Bracula: Condemor II",
+    duration: 192,
+    categories: ["comedia", "aventura"],
+  },
+  {
+    title: "Spider-Man: No Way Home",
+    duration: 122,
+    categories: ["aventura", "acción"],
+  },
+  {
+    title: "The Voices",
+    duration: 223,
+    categories: ["comedia", "thriller"],
+  },
+  {
+    title: "Shrek",
+    duration: 111,
+    categories: ["comedia", "aventura", "animación"],
+  },
+];
+
+
+const categorias = [];
+
+
+for (let movie of movies) {
+  for (let category of movie.categories) {
+    
+    if (!categorias.includes(category)) {
+      categorias.push(category); 
+    }
+  }
+}
+
+
+console.log("Categorías de películas:");
+console.log(categorias);
