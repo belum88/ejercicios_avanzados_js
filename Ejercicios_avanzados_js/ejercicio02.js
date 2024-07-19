914e95a4d5aec99723e89969f67167534c8938a3
Abel <abel.fernandez.martinez@gmail.com>
2024-07-19 23:57:52 +0200

    Create ejercicio02.js

---
+++ Ejercicios_avanzados_js/ejercicio02.js
@@ -0,0 +1,46 @@
+const users = [
+    {
+      name: "Alberto",
+      favoritesSounds: {
+        waves: { format: "mp3", volume: 50 },
+        rain: { format: "ogg", volume: 60 },
+        firecamp: { format: "mp3", volume: 80 },
+      },
+    },
+    {
+      name: "Antonio",
+      favoritesSounds: {
+        waves: { format: "mp3", volume: 30 },
+        shower: { format: "ogg", volume: 55 },
+        train: { format: "mp3", volume: 60 },
+      },
+    },
+    {
+      name: "Santiago",
+      favoritesSounds: {
+        shower: { format: "mp3", volume: 50 },
+        train: { format: "ogg", volume: 60 },
+        firecamp: { format: "mp3", volume: 80 },
+      },
+    },
+    {
+      name: "Laura",
+      favoritesSounds: {
+        waves: { format: "mp3", volume: 67 },
+        wind: { format: "ogg", volume: 35 },
+        firecamp: { format: "mp3", volume: 60 },
+      },
+    },
+  ];
+  
+  function soundAverage(userList) {
+    let soundSum = 0;
+    for (let user of userList) {
+      for (let sound in user["favoritesSounds"]) {
+        //Atacamos al favoritesounds -> key = sound / value = volume
+        soundSum += user["favoritesSounds"][sound].volume;
+      }
+    }
+    return soundSum;
+  }
+  console.log(soundAverage(users));
