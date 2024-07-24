f55f323880b76debcd92589fbfe8a19715c65790
Abel <abel.fernandez.martinez@gmail.com>
2024-07-24 18:23:18 +0200

    Create ejercicio04.js

---
+++ Ejercicios_avanzados_js/ejercicio04.js
@@ -0,0 +1,41 @@
+const mainCharacters = [
+    "Luke",
+    "Leia",
+    "Han Solo",
+    "Chewbacca",
+    "Rey",
+    "Anakin",
+    "Obi-Wan",
+  ];
+  
+  //1.
+  function findArrayIndex(array, text) {
+    let index = null;
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] === text) {
+        index = i;
+        //Salir del bucle si lo encuentra
+        break;
+      }
+    }
+    if (index === null) {
+      return null;
+    }
+    return index;
+  }
+  
+  //2.
+  function removeItem(array, text) {
+    const index = findArrayIndex(array, text);
+    if (index === null) {
+      return "No se ha encontrado el índice";
+    }
+    array.splice(index, 1);
+  
+    return array;
+  }
+  
+  console.log(
+    "Esta es la lista con tu elemento eliminado: \n",
+    removeItem(mainCharacters, "Rey")
+  );
